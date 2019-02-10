b0e99248009ff7c17841fe37bbca1f89ae08e095
landjbs <landonsmith@college.harvard.edu>
2019-02-09 19:50:59 -0500

    base code established

--- application.py
+++ application.py
@@ -1,1 +1,10 @@
+import os
+from flask import Flask, flash, jsonify, redirect, render_template, request, session
+from flask_session import Session
+import requests
 
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template('index.html')
